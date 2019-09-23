74f4d938ca57981097b187213295fb1a249936d8
Muhammad Ihsan <55564001+ihsan946@users.noreply.github.com>
2019-09-23 14:37:35 +0700

    Add files via upload

---
+++ IF110118037latihan1biodata/src/IF110118037latihan1biodata/IF110118037latihan1biodata.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package IF110118037latihan1biodata;
+
+/**
+ * @author 
+ * NAMA      : Muhammad Ihsan
+ * KELAS     : IF-1
+ * NIM       : 10118037
+ * Deskripsi Program   : Program ini berisi program untuk menampilkan
+ *                       biodata. 
+ */
+public class IF110118037latihan1biodata {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("BIODATA ROCKSTAR" + "\n" + 
+                "-----------------------------------------------");
+        System.out.println("NAMA\t\t: Muhammad Ihsan");
+        System.out.println("ALAMAT\t\t: Jalan Tangkuban Perahu No.21");
+        System.out.println("TEMPAT LAHIR\t: Bandung");
+        System.out.println("TANGGAL LAHIR\t: 23 Desember 1999");
+        System.out.println("-----------------------------------------------");
+        System.out.println("HOBI\t\t: Olahraga");
+        System.out.println("MAKES\t\t: Mendoan");
+        System.out.println("MIKES\t\t: Teh Manis");
+    }
+    
+}
